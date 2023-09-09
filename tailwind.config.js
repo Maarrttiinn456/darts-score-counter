bc5965765116493e08b2092e5a48ecd839263a67
Martin Schusterm <schustermartin23@gmail.com>
2023-09-09 18:23:30 +0200

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '8px'
+    },
+    colors: {
+      'primary': {
+          '50': '#f6f6f9',
+          '100': '#ededf1',
+          '200': '#d7d8e0',
+          '300': '#b3b6c6',
+          '400': '#8a8ea6',
+          '500': '#6c718b',
+          '600': '#565973',
+          '700': '#47495d',
+          '800': '#3c3e4e',
+          '900': '#363744',
+          '950': '#24242d',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
